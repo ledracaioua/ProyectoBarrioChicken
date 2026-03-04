bc53ca09f1c50070a983ec9414a25e43a65500d0
ledracaioua <caio.ledra@cloud.uautonoma.cl>
2026-03-03 21:32:44 -0300

    Update categories.ts

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/categories';
+const BASE_URL = 'https://proyectobarriochicken.onrender.com/api/categories';
 
 export const getCategories = () => axios.get<string[]>(BASE_URL);
